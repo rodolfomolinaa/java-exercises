93dce94184cf8391b519d76281153aa16f9b0552
Rodolfo <rodolfomolinasv@gmail.com>
2021-03-17 20:24:36 -0600

    Create Main and WeekOne clasess

---
+++ CoreCode.java
@@ -0,0 +1,5 @@
+public class CoreCode {
+    public static void main(String[] args) {
+        WeekOne.exerciseOne("Your fix the errors!");
+    }
+}
---
+++ WeekOne.java
@@ -0,0 +1,14 @@
+public class WeekOne {
+    public static void exerciseOne(String msg) {
+        Integer[] numbers = { 0, 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        System.out.println(msg);
+    }
+
+    public static void exerciseTwo() {
+        System.out.println("test");
+    }
+}
